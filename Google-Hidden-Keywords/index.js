a400b16a1eebd7db9e2497e82aa4cd7d2c1f384e
erwin solihin <ewwink@users.noreply.github.com>
2019-01-21 19:10:17 +0700

    Update index.js

--- Google-Hidden-Keywords/index.js
+++ Google-Hidden-Keywords/index.js
@@ -5,8 +5,7 @@ V 1.0 - init, Sep 24, 2018
 */
 
 // bookmarklet, compressed code
-javascript:(function(){for(var d="",a=document.querySelectorAll("div[data-base-uri] > div[jsname]"),b=0;b<a.length;b++){a[b].querySelector("div[jsname]").style="display:inherit;";a[b].style="display:inherit; opacity:1; border:1px solid #bbb; padding:5px; font-weight:bold; margin-left:0px; background:#eaeac3;";for(var e=a[b].querySelectorAll("div[data-ved]"),c=0;c<e.length;c++)d+=e[c].textContent+"\r\n"}a=document.createElement("textarea");a.value=d;a.spellcheck=!1;a.onclick=function(){this.select()};a.style=
-"width: 400px;height: 300px;position: absolute;top: 125px;z-index: 9999;right:0;white-space: pre;";document.body.appendChild(a)})();
+javascript:(function(){for(var d="",a=document.querySelectorAll("div[data-base-uri] > div[jsname]"),b=0;b<a.length;b++){a[b].querySelector("div[jsname]").style="display:inherit;";a[b].style="display:inherit; opacity:1; border:1px solid #bbb; padding:5px; font-weight:bold; margin-left:0px; background:#eaeac3;";for(var e=a[b].querySelectorAll("div[data-ved]"),c=0;c<e.length;c++)d+=e[c].textContent+"\r\n"}a=document.createElement("textarea");a.value=d;a.spellcheck=!1;a.onclick=function(){this.select()};a.style="width: 400px;height: 300px;position: absolute;top: 125px;z-index: 9999;right:0;white-space: pre;";document.body.appendChild(a)})();
 
 // pretty print code
 (function() {
