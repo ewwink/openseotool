fa770d7e96eb09f97a1eb69f8cb86ccd4ee5c03b
erwin solihin <ewwink@users.noreply.github.com>
2019-01-21 19:09:49 +0700

    Update index.js

--- Google-Hidden-Keywords/index.js
+++ Google-Hidden-Keywords/index.js
@@ -1,16 +1,21 @@
+/*
+Update:
+V 1.1 - Jan 21, 2019
+V 1.0 - init, Sep 24, 2018
+*/
+
 // bookmarklet, compressed code
-javascript:(function(){for(var d="",a=document.getElementsByTagName("div"),b=0;b<a.length;b++)if(/eobd_\d+/.test(a[b].id)){a[b].style="display: inherit;border: 1px solid #bbb;padding: 5px;font-weight: bold;margin-left: 15px;background: #eaeac3;";for(var e=a[b].getElementsByTagName("div"),c=0;c<e.length;c++)d+=e[c].textContent+"\r\n"}else/eobm_\d+/.test(a[b].id)&&(a[b].style="display:inherit;opacity:1;");a=document.createElement("textarea");a.value=d;a.spellcheck=!1;a.onclick=function(){this.select()};a.style="width: 400px;height: 300px;position: absolute;top: 125px;z-index: 9999;right:0;white-space: pre;";document.body.appendChild(a)})();
+javascript:(function(){for(var d="",a=document.querySelectorAll("div[data-base-uri] > div[jsname]"),b=0;b<a.length;b++){a[b].querySelector("div[jsname]").style="display:inherit;";a[b].style="display:inherit; opacity:1; border:1px solid #bbb; padding:5px; font-weight:bold; margin-left:0px; background:#eaeac3;";for(var e=a[b].querySelectorAll("div[data-ved]"),c=0;c<e.length;c++)d+=e[c].textContent+"\r\n"}a=document.createElement("textarea");a.value=d;a.spellcheck=!1;a.onclick=function(){this.select()};a.style=
+"width: 400px;height: 300px;position: absolute;top: 125px;z-index: 9999;right:0;white-space: pre;";document.body.appendChild(a)})();
 
 // pretty print code
 (function() {
-  for (var d = "", a = document.getElementsByTagName("div"), b = 0; b < a.length; b++) {
-    if (/eobd_\d+/.test(a[b].id)) {
-      a[b].style = "display: inherit;border: 1px solid #bbb;padding: 5px;font-weight: bold;margin-left: 15px;background: #eaeac3;";
-      for (var e = a[b].getElementsByTagName("div"), c = 0; c < e.length; c++) {
-        d += e[c].textContent + "\r\n";
-      }
-    } else {
-      /eobm_\d+/.test(a[b].id) && (a[b].style = "display:inherit;opacity:1;");
+  var selector = 'div[data-base-uri] > div[jsname]';
+  for (var d = "", a = document.querySelectorAll(selector), b = 0; b < a.length; b++) {
+    a[b].querySelector("div[jsname]").style = "display:inherit;"
+    a[b].style = "display:inherit; opacity:1; border:1px solid #bbb; padding:5px; font-weight:bold; margin-left:0px; background:#eaeac3;";
+    for (var e = a[b].querySelectorAll("div[data-ved]"), c = 0; c < e.length; c++) {
+      d += e[c].textContent + "\r\n";
     }
   }
   a = document.createElement("textarea");
@@ -22,3 +27,4 @@ javascript:(function(){for(var d="",a=document.getElementsByTagName("div"),b=0;b
   a.style = "width: 400px;height: 300px;position: absolute;top: 125px;z-index: 9999;right:0;white-space: pre;";
   document.body.appendChild(a);
 })();
+
